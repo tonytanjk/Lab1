83f9eb2ba751c7b1980f9c5499200e104f4aea88
Ernest <ERNESTHO09.24@ichat.sp.edu.sg>
2024-04-18 13:30:17 +0800

    Code changed by Ernest

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,138 @@
-print("ET0735 - DevOps for AIot")
-print("DCPE year-2 CES path only")
+word = input()
+add = ''
+ak = 0
+bk = 0
+ck = 0
+dk = 0
+ek = 0
+fk = 0
+gk = 0
+hk = 0
+ik = 0
+jk = 0
+kk = 0
+lk = 0
+mk = 0
+nk = 0
+ok = 0
+pk = 0
+qk = 0
+rk = 0
+sk = 0
+tk = 0
+uk = 0
+vk = 0
+wk = 0
+xk = 0
+yk = 0
+zk = 0
+
+
+for x in word:
+    if x == 'a':
+        ak += 1
+    elif x == 'b':
+        bk += 1
+    elif x == 'c':
+        ck += 1
+    elif x == 'd':
+        dk += 1
+    elif x == 'e':
+        ek += 1
+    elif x == 'f':
+        fk += 1
+    elif x == 'g':
+        gk += 1
+    elif x == 'h':
+        hk += 1
+    elif x == 'i':
+        ik += 1
+    elif x == 'j':
+        jk += 1
+    elif x == 'k':
+        kk += 1
+    elif x == 'l':
+        lk += 1
+    elif x == 'm':
+        mk += 1
+    elif x == 'n':
+        nk += 1
+    elif x == 'o':
+        ok += 1
+    elif x == 'p':
+        pk += 1
+    elif x == 'q':
+        qk += 1
+    elif x == 'r':
+        rk += 1
+    elif x == 's':
+        sk += 1
+    elif x == 't':
+        tk += 1
+    elif x == 'u':
+        uk += 1
+    elif x == 'v':
+        vk += 1
+    elif x == 'w':
+        wk += 1
+    elif x == 'x':
+        xk += 1
+    elif x == 'y':
+        yk += 1
+    elif x == 'z':
+        zk += 1
+
+if ak > 1:
+    print('Deja Vu')
+elif bk > 1:
+    print('Deja Vu')
+elif ck > 1:
+    print('Deja Vu')
+elif dk > 1:
+    print('Deja Vu')
+elif ek > 1:
+    print('Deja Vu')
+elif fk > 1:
+    print('Deja Vu')
+elif gk > 1:
+    print('Deja Vu')
+elif hk > 1:
+    print('Deja Vu')
+elif ik > 1:
+    print('Deja Vu')
+elif jk > 1:
+    print('Deja Vu')
+elif kk > 1:
+    print('Deja Vu')
+elif lk > 1:
+    print('Deja Vu')
+elif mk > 1:
+    print('Deja Vu')
+elif nk > 1:
+    print('Deja Vu')
+elif ok > 1:
+    print('Deja Vu')
+elif pk > 1:
+    print('Deja Vu')
+elif qk > 1:
+    print('Deja Vu')
+elif rk > 1:
+    print('Deja Vu')
+elif sk > 1:
+    print('Deja Vu')
+elif tk > 1:
+    print('Deja Vu')
+elif uk > 1:
+    print('Deja Vu')
+elif vk > 1:
+    print('Deja Vu')
+elif wk > 1:
+    print('Deja Vu')
+elif xk > 1:
+    print('Deja Vu')
+elif yk > 1:
+    print('Deja Vu')
+elif zk > 1:
+    print('Deja Vu')
+else:
+    print('Unique')
